f3ec50a5c115960301218e8a1485d9481cfd1841
Ash <ashleyhsieh.pl@gmail.com>
2022-01-20 15:34:46 +0800

    New

--- FrenchDispatch/ViewController.swift
+++ FrenchDispatch/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var storiesImageView: UIImageView!
+    let stories = ["inBrief", "#1", "#2", "#3"]
+    var index = 0
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func next(_ sender: Any) {
+        index = (index + 1) % stories.count
+        let storyName = stories[index]
+        storiesImageView.image = UIImage(named: storyName)
+        pageControl.currentPage = index
+        segmentedControl.selectedSegmentIndex = index
+    }
+    
+    @IBAction func previous(_ sender: Any) {
+        index = (index + stories.count - 1) % stories.count
+        let storyName = stories[index]
+        storiesImageView.image = UIImage(named: storyName)
+        pageControl.currentPage = index
+        segmentedControl.selectedSegmentIndex = index
+    }
+    
 
+    @IBAction func pageControlTapped(_ sender: UIPageControl) {
+        index = sender.currentPage
+        let storyName = stories[index]
+        storiesImageView.image = UIImage(named: storyName)
+        segmentedControl.selectedSegmentIndex = index
+    }
+    
+    @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+        index = sender.selectedSegmentIndex
+        let storyName = stories[index]
+        storiesImageView.image = UIImage(named: storyName)
+        pageControl.currentPage = index
+    }
+    
 }
 
